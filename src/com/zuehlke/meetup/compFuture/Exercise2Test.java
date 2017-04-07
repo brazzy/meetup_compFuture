37dd6f29b932ce4b690e791fb9171bab4ba0bf96
Michael Borgwardt <mibo@zuehlke.com>
2017-04-07 15:41:56 +0200

    add second exercise and separate tests

--- src/com/zuehlke/meetup/compFuture/Exercise2Test.java
+++ src/com/zuehlke/meetup/compFuture/Exercise2Test.java
@@ -1,35 +1,20 @@
 package com.zuehlke.meetup.compFuture;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.junit.Test;
 
 public class Exercise2Test {
 	private WikiCrawler testee = new WikiCrawler();
-	
-	@Test
-	public void getCycle() throws Exception{
-		CompletableFuture<List<String>> result = testee.getCycle("Revolution");
-		assertEquals(Arrays.asList("Louvre", "Palais du Louvre", "Glaspyramide im Innenhof des Louvre"), result.get());
-	}
-	
-	@Test
-	public void asynchronous() throws Exception{
-		long start = System.currentTimeMillis();
-		testee.getCycle("Revolution");
-		long elapsed = System.currentTimeMillis() - start;
-		assertTrue(String.format("Call took too %s milliseconds, cannot have been asynchronous.", elapsed), elapsed < 10);
-	}
 
 	@Test
-	public void error() throws Exception{
-		CompletableFuture<List<String>> result = testee.getCycle("");
-		assertTrue("Did not complete exceptionally", result.isCompletedExceptionally());
+	public void getFirstLinkInArticle() throws Exception{
+		CompletableFuture<String> result = testee.getFirstLinkInArticle("Welt");
+		assertEquals("Totalität", result.get());
+		result = testee.getFirstLinkInArticle("München");
+		assertEquals("Oberbayern", result.get());
 	}
 	
 }
