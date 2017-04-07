1f995f4e50995976ddd508a426186989bb26aad7
Michael Borgwardt <mibo@zuehlke.com>
2017-04-07 10:11:38 +0200

    use Completeable future wrongly and test asynchronity

--- src/com/zuehlke/meetup/compFuture/CompletableFutureTest.java
+++ src/com/zuehlke/meetup/compFuture/CompletableFutureTest.java
@@ -7,12 +7,20 @@ import static org.junit.Assert.*;
 
 public class CompletableFutureTest {
 
+	private static final String START_URL = "https://de.wikipedia.org/wiki/Java_(Programmiersprache)";
 	private Downloader testee = new Downloader();
 	
 	@Test
 	public void downloadContent() throws Exception{
-		String content = testee.download("https://de.wikipedia.org/wiki/Java_(Programmiersprache)");
+		String content = testee.download(START_URL).get();
 		assertThat(content, CoreMatchers.containsString("Java Technology: The Early Years."));
 	}
 
+	@Test
+	public void downloadAsynchronous() throws Exception{
+		long start = System.currentTimeMillis();
+		testee.download(START_URL).get();
+		long elapsed = System.currentTimeMillis() - start;
+		assertTrue(String.format("Call took too %s milliseconds, cannot have been asynchronous.", elapsed), elapsed < 10);
+	}
 }
--- src/com/zuehlke/meetup/compFuture/Downloader.java
+++ src/com/zuehlke/meetup/compFuture/Downloader.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 
 public class Downloader {
-	public String download(String url) throws IOException{
+	public CompletableFuture<String> download(String url) throws IOException{
+		CompletableFuture<String> result = new CompletableFuture<>();
 		try(InputStream stream = new URL(url).openStream()){
-			return new Scanner(stream, "UTF-8").useDelimiter("\\A").next();			
+			result.complete(new Scanner(stream, "UTF-8").useDelimiter("\\A").next());
+			return result;
 		}
 	}
 }
